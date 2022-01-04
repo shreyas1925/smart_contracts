ceec95b097fe4e98e500de733d5ea645b2385692
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2022-01-04 18:43:12 +0530

    started testing the contract using mocha

--- node_contract/compile.js
+++ node_contract/compile.js
@@ -4,5 +4,7 @@ const solc = require("solc");
 const inBoxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
 const source = fs.readFileSync(inBoxPath, "utf8");
 
-console.log(solc.compile(source, 1));
+// console.log(solc.compile(source, 1));
 // console.log(source);
+
+module.exports = solc.compile(source, 1).contracts[":Inbox"];
---
+++ node_contract/test/Inbox.test.js
@@ -0,0 +1,5 @@
+// writing tests which calls the functions
+
+const assert = require("assert"); //nodejs runtime built in library to check whether this some value equal to another value
+const ganache = require("ganache-cli");
+const Web3 = require("web3"); // Web3 is a constructor which provides a instance =s of its library so capital Web3
