65b63edc6dda262d6b827ba1544f3a4b712a45c8
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2022-01-05 23:09:14 +0530

    testing with mocha

--- node_contract/test/Inbox.test.js
+++ node_contract/test/Inbox.test.js
@@ -7,3 +7,23 @@ const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider()); //making a instance with a provider along with ganache ( a network we are connecting)
+const { interface, bytecode } = require("../compile");
+let accounts;
+let inbox;
+beforeEach(async () => {
+  // Get a list of all accounts
+  // we are dealing with ethereum
+  accounts = await web3.eth.getAccounts();
+
+  //Use one of those accounts to deploy our contract
+
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Heyy !!"] })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    console.log(inbox);
+  });
+});
