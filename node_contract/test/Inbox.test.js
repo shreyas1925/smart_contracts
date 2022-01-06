bcba8f2fbbd267fd709fcc893dbba3c0f70af84b
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2022-01-07 01:02:22 +0530

    real time testing

--- node_contract/test/Inbox.test.js
+++ node_contract/test/Inbox.test.js
@@ -10,6 +10,7 @@ const web3 = new Web3(ganache.provider()); //making a instance with a provider a
 const { interface, bytecode } = require("../compile");
 let accounts;
 let inbox;
+const INITIAL_STRING = "Heyy Bro!!";
 beforeEach(async () => {
   // Get a list of all accounts
   // we are dealing with ethereum
@@ -18,12 +19,27 @@ beforeEach(async () => {
   //Use one of those accounts to deploy our contract
 
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ["Heyy !!"] })
+    .deploy({
+      data: bytecode,
+      arguments: [INITIAL_STRING],
+    })
     .send({ from: accounts[0], gas: "1000000" });
+  //through send I am doing a transaction
 });
 
 describe("Inbox", () => {
   it("deploys a contract", () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address); //ok i seeing is inbox.options.address defined value
+  });
+
+  it("has an default message", async () => {
+    // calling a method becoz i only want a data , if i need to change any data of a
+    //  contrasct information then I would have made a transaction to a contract
+
+    const message = await inbox.methods.message().call();
+    // if i had any paramters then i would have used it like in message()
+    // if it was a transcation then my call() would contain a object about who would pay and how uch gas we would have used
+
+    assert.equal(message, INITIAL_STRING);
   });
 });
