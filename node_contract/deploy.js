4a3d1cc1ea6c730a3ffa189a35388572894f5fdd
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2022-01-15 12:14:47 +0530

    deploying the contract

--- node_contract/deploy.js
+++ node_contract/deploy.js
@@ -13,7 +13,7 @@ const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
   console.log("Attempting to deploy from account", accounts[0]);
 
-  await new web3.eth.Contract(JSON.parse(interface))
+  const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({
       data: bytecode,
       arguments: ["hi shreyas"],
